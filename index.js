14442235539b337e4154e505fb1c3ac27eabc4c1
MaTaaN0615 <h.p._09@hotmail.com>
2014-11-05 01:16:47 +0700

    add author 1 colum

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/api/book', function(req, res){
 	var books = [
-		{title: 'Angular', price: 800},
-		{title: 'Node.js', price: 600},
-		{title: 'CSS', price: 500},
+		{title: 'Angular',artist: 'NUTCHA', price: 800},
+		{title: 'Node.js',artist: 'NATTY' , price: 600},
+		{title: 'CSS' ,artist: 'NOTCY', price: 500},
 
 	];
 	res.send(books);
